2ba7b4939d17872c12caa195c7341b3bb650b909
zeerah-dave <ziradavid8@gmail.com>
2023-06-16 11:37:58 -0700

    Alx C programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+*main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+    printf("Size of int: %d byte(s)\n", sizeof(int));
+    printf("Size of long int: %d byte(s)\n", sizeof(long int));
+    printf("Size of char: %d byte(s)\n", sizeof(char));
+    printf("Size of float: %d byte(s)\n", sizeof(float));
+    printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+    return (0);
+}
