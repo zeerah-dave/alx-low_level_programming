34db455339c0851fa13ca95eaba4554309f47873
zeerah-dave <ziradavid8@gmail.com>
2023-06-19 13:29:27 -0700

    Udated

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,14 +7,12 @@
  *
  * Return: Always 0 (Success)
  */
-/* betty style doc for function main goes there */
 int main(void)
 {	int lastDigit;
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	lastDigit = n % 10;
 	if (lastDigit > 5)
 	{
