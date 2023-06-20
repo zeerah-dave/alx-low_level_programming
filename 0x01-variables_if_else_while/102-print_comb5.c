a31fd3789bcca6286022ab5f8c2f151a02cfe857
zeerah-dave <ziradavid8@gmail.com>
2023-06-20 01:34:05 -0700

    Updated

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,32 +7,28 @@
  */
 int main(void)
 {
-       	int num1;
-	int num2;
-	for (num1 = 0; num1 < 100; num1++) 
+	int num_1;
+	int num_2;
+
+	for (num_1 = 0; num_1 < 100; num_1++)
 	{
-		for (num2 = num1; num2 < 100; num2++)
+		for (num_2 = 0; num_2 < 100; num_2++)
 		{
-			int num1_tens = num1 / 10;
-			int num1_ones = num1 % 10;
-			int num2_tens = num2 / 10;
-			int num2_ones = num2 % 10;
-			putchar(num1_tens / 10 + '0');
-			putchar(num1_tens % 10 + '0');
-			putchar(num1_ones / 10 + '0');
-			putchar(num1_ones % 10 + '0');
-			putchar(' ');
-			putchar(num2_tens / 10 + '0');
-			putchar(num2_tens % 10 + '0');
-			putchar(num2_ones / 10 + '0');
-			putchar(num2_ones % 10 + '0');
-			if (num1 != 99 || num2 != 99)
+			if (num_1 < num_2)
 			{
-				putchar(',');
+				putchar((num_1 / 10) + 48);
+				putchar((num_1 % 10) + 48);
 				putchar(' ');
+				putchar((num_2 / 10) + 48);
+				putchar((num_2 % 10) + 48);
+				if (i != 98 || num_2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
