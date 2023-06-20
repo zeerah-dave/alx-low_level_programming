ac55583a7b9dd2d46d2d75110c175393f5b4c7ab
zeerah-dave <ziradavid8@gmail.com>
2023-06-19 18:03:04 -0700

    Updated

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,7 +10,6 @@ int main(void)
 	int hundredsDigit;
 	int tensDigit;
 	int onesDigit;
-
 	for (hundredsDigit = 0; hundredsDigit < 10; hundredsDigit++)
 	{
 		for (tensDigit = hundredsDigit + 1; tensDigit < 10; tensDigit++)
@@ -29,5 +28,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-       	return (0);
+	return (0);
 }
