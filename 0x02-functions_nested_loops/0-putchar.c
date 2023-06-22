d8f3ea4431479a86d341b4567865938bd0d5b8fd
zeerah-dave <ziradavid8@gmail.com>
2023-06-22 12:22:59 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+	char my_char[] = "_putchar";
+	int len_char = strlen(my_char);
+
+	for (i = 0; i < len_char; i++)
+	{
+		putchar(my_char[i]);
+	}
+	putchar('\n');
+	return (0);
+}
